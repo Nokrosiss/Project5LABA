b43d84ec598820ab3a2fa05f7008f6d0cd5d4a43
Nokrosis <Nokrosis22@yandex.ru>
2023-12-12 12:22:33 +0300

    Start

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace StudentsApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetStudents()
+    {
+        // Dummy data for demonstration purposes
+        var students = new List<Student>
+        {
+            new Student { Name = "John Doe", Marks = 90 },
+            new Student { Name = "Jane Doe", Marks = 85 },
+            new Student { Name = "Bob Smith", Marks = 78 }
+            // Add more students as needed
+        };
+
+        return Ok(students);
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+    }
+}
---
+++ MarksController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MarksApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MicroserviceController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public MicroserviceController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("StudentsServiceClient");
+    }
+
+    [HttpGet("students")]
+    public async Task<IActionResult> GetStudentsFromOtherMicroservice()
+    {
+        try
+        {
+            // Make a request to the Students microservice's endpoint
+            var response = await _httpClient.GetAsync("https://localhost:7130/Students");
+
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                // Read and return the response content
+                var students = await response.Content.ReadFromJsonAsync<Student[]>();
+                return Ok(students);
+            }
+            else
+            {
+                // Handle error cases
+                return StatusCode((int)response.StatusCode, "Error retrieving students");
+            }
+        }
+        catch (HttpRequestException)
+        {
+            // Handle network-related errors
+            return StatusCode(500, "Error communicating with the Students microservice");
+        }
+    }
+
+    // Other actions for the microservice can be added here
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+    }
+}
